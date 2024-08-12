9f654f9681d350f8e36fa99810aba410ef8f4ca9
Naman Rudrakshi <142565166+Naman-Rudrakshi@users.noreply.github.com>
2024-08-11 23:08:54 -0400

    Add files via upload

---
+++ source/test/java/com/jay9971/VTBuilder/VtBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay9971.VTBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
